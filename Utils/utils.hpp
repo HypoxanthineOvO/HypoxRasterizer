91d338fa4ec1213edcfbf8a120d0982237e289df
HypoxanthineOvO <hyx021203@163.com>
2024-10-16 17:22:20 +0800

    Add some print functions

--- Utils/utils.hpp
+++ Utils/utils.hpp
@@ -2,9 +2,24 @@
 #define UTILS_HPP_
 
 #include "cores.hpp"
+#include <iostream>
 
 namespace utils {
-
+    // Print Function for Eigen Types
+    static void printVec(const VecXf& vec) {
+        for (int i = 0; i < vec.size(); i++) {
+            printf("%f ", vec(i));
+        }
+        printf("\n");
+    }
+    static void printMat(const MatXf& mat) {
+        for (int i = 0; i < mat.rows(); i++) {
+            for (int j = 0; j < mat.cols(); j++) {
+                printf("%f ", mat(i, j));
+            }
+            printf("\n");
+        }
+    }
 };
 
 #endif // UTILS_HPP_
