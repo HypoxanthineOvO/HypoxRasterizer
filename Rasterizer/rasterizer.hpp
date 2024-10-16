a30e53de4276c46ee543e960cf11a83467b80ff1
HypoxanthineOvO <hyx021203@163.com>
2024-10-16 19:42:33 +0800

    Add Rasterization Basic Functions

--- Rasterizer/rasterizer.hpp
+++ Rasterizer/rasterizer.hpp
@@ -1,1 +1,22 @@
+#ifndef RASTERIZER_HPP_
+#define RASTERIZER_HPP_
+
 #include "utils.hpp"
+
+class Rasterizer {
+public:
+    /* Constructors */
+    Rasterizer();
+
+    // Pass
+    void Pass();
+    void VertexProcessing();
+    void TriangleProcessing();
+    void Rasterization();
+    void FragmentProcessing();
+    void DisplayToImage();
+private:
+};
+
+
+#endif // RASTERIZER_HPP_
