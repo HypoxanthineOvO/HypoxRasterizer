a1144b0fe2ef350aa667afad3c9c3436879df1d9
HypoxanthineOvO <hyx021203@163.com>
2024-10-16 16:41:10 +0800

    Add Setters and Getters

--- Modules/Camera/camera.hpp
+++ Modules/Camera/camera.hpp
@@ -18,6 +18,18 @@ public:
     // Get Matrix Functions
     Mat4f getViewMatrix();
     Mat4f getProjectionMatrix();
+
+    // Setters
+    void setFocalLength(float f) { focal_len = f; }
+    void setFOV(float f) { fov = f; }
+    
+    // Getters
+    Vec3f getPosition() { return position; }
+    Vec3f getForward() { return forward; }
+    Vec3f getRight() { return right; }
+    Vec3f getUp() { return up; }
+    float getFocalLength() { return focal_len; }
+    float getFOV() { return fov; }
 private:
     // Extrinsic Parameters
     Vec3f position;
