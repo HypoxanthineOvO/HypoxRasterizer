66941134c212f9b904201496f7b999bad1f9c369
HypoxanthineOvO <hyx021203@163.com>
2024-10-17 21:05:06 +0800

    Add framework of object

--- Modules/Object/object.hpp
+++ Modules/Object/object.hpp
@@ -1,7 +1,29 @@
 #ifndef OBJECT_HPP_
 #define OBJECT_HPP_
 
+#include "geometry.hpp"
 
+class Object {
+public:
+    Object();
+    Object(const std::string& file_name) {
+        loadObject(file_name);
+    };
+
+    /* Modify Functions */
+    void addTriangle(Triangle tri) { triangles.push_back(tri); }
+    void loadObject(const std::string& file_name);
+
+    /* Transform Functions */
+    void localToWorld(const Mat4f& model_matrix);
+private:
+    /* Raw Data in Local Space */
+    std::vector<Triangle> triangles_local;
+    
+    /* Data for Rendering */
+    Mat4f model_matrix;
+    std::vector<Triangle> triangles;
+};
 
 
 
