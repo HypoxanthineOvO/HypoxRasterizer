05dd3ee36f85dc15c01bc602ecb612f2e41400a0
HypoxanthineOvO <hyx021203@163.com>
2024-11-01 17:07:33 +0800

    Add some implementation.

--- Utils/Configs/configs.cpp
+++ Utils/Configs/configs.cpp
@@ -1,1 +1,28 @@
 #include "configs.hpp"
+#include <fstream>
+#include "nlohmann/json.hpp"
+
+float loadFloat(const nlohmann::json& j) {
+    float v;
+    j.get_to(v);
+    return v;
+}
+
+Vec2i loadVec2i(const nlohmann::json& j) {
+    int x, y;
+    j[0].get_to(x), j[1].get_to(y);
+    return Vec2i(x, y);
+}
+
+Vec3f loadVec3f(const nlohmann::json& j) {
+    float x, y, z;
+    j[0].get_to(x), j[1].get_to(y), j[2].get_to(z);
+    return Vec3f(x, y, z);
+}
+
+void Config::loadConfig(const std::string& config_file_path) {
+    std::ifstream raw_file(config_file_path);
+    nlohmann::json raw;
+    raw_file >> raw;
+
+}
